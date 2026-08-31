439fabdf3d2b1098dd9d675dd848114584208ad0
Pierre Alexandre <arsantos506@gmail.com>
2026-08-31 15:25:07 -0300

    first commit

---
+++ src/main/java/com/tutorials/projetotutor/ProjetotutorApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.projetotutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotutorApplication.class, args);
+	}
+
+}
