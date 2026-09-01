5b7b1e0dc500158a598a422eda5145de5d989aae
Pierre Alexandre <arsantos506@gmail.com>
2026-09-01 16:59:38 -0300

    feat(TutorialRepository.java):Add do controller e os metodos get e post.

--- src/main/java/com/tutorials/projetotutor/controller/TutorialController.java
+++ src/main/java/com/tutorials/projetotutor/controller/TutorialController.java
@@ -1,4 +1,65 @@
 package com.tutorials.projetotutor.controller;
 
+import com.tutorials.projetotutor.model.TutorialModel;
+import com.tutorials.projetotutor.repository.TutorialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
 public class TutorialController {
+
+    @Autowired
+    TutorialRepository tutorialRepository;
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<TutorialModel>> getAllTutorials(@RequestParam(required = false) String title) {
+        try {
+            List<TutorialModel> tutorials = new ArrayList<TutorialModel>();
+
+            if (title == null)
+                tutorialRepository.findAll().forEach(tutorials::add);
+            else
+                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>((HttpHeaders) null, (HttpStatusCode) HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/tutorials/{id}")
+    public ResponseEntity<TutorialModel> getTutorialById(@PathVariable("id") Long id) {
+        Optional<TutorialModel> tutorialData = tutorialRepository.findById(id);
+
+        if (tutorialData.isEmpty()) {
+            return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/tutorials")
+    public ResponseEntity<TutorialModel> createTutorial(@RequestBody TutorialModel tutorial) {
+        try {
+            TutorialModel rspsotatutorial = tutorialRepository
+                    .save(tutorial);
+            return new ResponseEntity<>(tutorial, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>((HttpHeaders) null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
