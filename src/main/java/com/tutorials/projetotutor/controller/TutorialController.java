84c2ad1384ae59ea7f04f5987249df618213fa88
Pierre Alexandre <arsantos506@gmail.com>
2026-09-02 16:29:36 -0300

    feat(TutorialController): Add os metodos update e delete.

--- src/main/java/com/tutorials/projetotutor/controller/TutorialController.java
+++ src/main/java/com/tutorials/projetotutor/controller/TutorialController.java
@@ -45,7 +45,7 @@ public class TutorialController {
     public ResponseEntity<TutorialModel> getTutorialById(@PathVariable("id") Long id) {
         Optional<TutorialModel> tutorialData = tutorialRepository.findById(id);
 
-        if (tutorialData.isEmpty()) {
+        if (tutorialData.isPresent()) {
             return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -60,6 +60,58 @@ public class TutorialController {
             return new ResponseEntity<>(tutorial, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>((HttpHeaders) null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/tutorials/{id}")
+    public ResponseEntity<TutorialModel> updateTutorial(@PathVariable("id") Long id, @RequestBody TutorialModel tutorial) {
+
+        Optional<TutorialModel> tutorialData = tutorialRepository.findById(id);
+
+        if (tutorialData.isPresent()) {
+            TutorialModel rspsotutorial = tutorialData.get();
+
+            rspsotutorial.setTitle(tutorial.getTitle());
+            rspsotutorial.setDescription(tutorial.getDescription());
+            rspsotutorial.setPublished(tutorial.getPublished());
+
+            return new ResponseEntity<>(tutorialRepository.save(rspsotutorial), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/tutorials/{id}")
+    public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable ("id") Long id) {
+        try {
+            tutorialRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/tutorials")
+    public ResponseEntity<HttpStatus> deleteAllTutorials() {
+        try {
+            tutorialRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/tutorials/published")
+    public ResponseEntity<List<TutorialModel>> findByPublished() {
+        try {
+            List<TutorialModel> tutorials = tutorialRepository.findByPublished(true);
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
