4357f4201a9a2630aba5f09572f51be0320135b4
Pierre Alexandre <arsantos506@gmail.com>
2026-09-03 16:21:20 -0300

    fix(TutorialRepository): utilizando o "JPQL" para realizar buscas manualmente.

--- src/main/java/com/tutorials/projetotutor/repository/TutorialRepository.java
+++ src/main/java/com/tutorials/projetotutor/repository/TutorialRepository.java
@@ -2,11 +2,25 @@ package com.tutorials.projetotutor.repository;
 
 import com.tutorials.projetotutor.model.TutorialModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.JpqlQueryBuilder;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TutorialRepository extends JpaRepository<TutorialModel, Long> {
+//    Optional<TutorialModel> findById (Long id);
+
     List<TutorialModel> findByPublished(boolean published);
 
-    List<TutorialModel> findByTitleContaining(String title);
+    @Query("SELECT t FROM TutorialModel t " +
+            "WHERE LOWER(t.title) LIKE LOWER(CONCAT('%', :title, '%'))")
+    List<TutorialModel> findPorTitle(String title);
+
+    List<TutorialModel> id(long id);
+
+    List<TutorialModel> findByTitle(String title);
+
+//    Long id(long id);
 }
